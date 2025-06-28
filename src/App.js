f83420e2d16d53606d5ac0ba36412417f15ed7b4
caruazu <gustavocaruazu@gmail.com>
2025-06-28 17:34:37 -0300

    adição de imagens e primeiro componente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './components/banner/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Organo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner></Banner>
+      <p>Organo</p>
     </div>
   );
 }
---
+++ src/components/banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return(
+        <header className="banner">
+            <img src="/img/banner.png" alt="imagem com funcionários unindo as mãos"/>
+        </header>
+    );
+}
+
+export default Banner;
